fc1e65529d1fd5617b7e0056bd861b3a0748227e
erik <samenai@pm.me>
2025-06-18 11:27:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', (c) => {
+  console.log(`${c.user.tag} is connected.`);
+});
+client.on('messageCreate', (msg) => {
+  if (msg.author.bot) {
+    return;
+  }
+  console.log(msg);
+});
+client.login(process.env.TOKEN);
---
+++ register-commands.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'pong!'
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Registring slash commands...');
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+    console.log('Slash commands were registred succesfully!');
+  } catch (error) {
+    console.log(`there was an error: ${error}`);
+  }
+})();
